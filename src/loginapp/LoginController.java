bea9c55f40a601d8c56ee0746ae4134de5d4c098
Mohamed Bensaleh <mobensaleh@outlook.com>
2021-03-04 11:21:37 -0700

    Connected to Database and added combo box to select between different user roles

--- src/loginapp/LoginController.java
+++ src/loginapp/LoginController.java
@@ -1,5 +1,39 @@
 package loginapp;
 
-public class LoginController {
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    LoginModel loginModel = new LoginModel();
+
+    @FXML
+    private Label dbstatus;
+    @FXML
+    private TextField idNumber;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private ComboBox<option> selection;
+    @FXML
+     private Button loginButton;
+
+    public void initialize(URL url, ResourceBundle rb){
+        if(this.loginModel.isDatabaseConnected()){
+            this.dbstatus.setText("Connected to Database");
+        }
+        else{
+            this.dbstatus.setText("Not Connected to Database");
+        }
+
+        this.selection.setItems(FXCollections.observableArrayList(option.values()));
+
+
+    }
 
 }
