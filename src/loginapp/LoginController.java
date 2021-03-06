3a534134e58eb9a9e72ca34e084c3afe18c9874e
Mohamed Bensaleh <mobensaleh@outlook.com>
2021-03-05 17:14:56 -0700

    Designed the add flight gui for Airline Manager

--- src/loginapp/LoginController.java
+++ src/loginapp/LoginController.java
@@ -59,8 +59,6 @@ public class LoginController implements Initializable {
 
 
                 }
-
-
             }
             else{
                 this.loginStatus.setText("INVALID Login Info");
