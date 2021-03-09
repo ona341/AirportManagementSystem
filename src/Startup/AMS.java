3f6850fb7a3157c3e72401a695a186843cfc9ba7
Blake Stadnyk <blake.stadnyk@usask.ca>
2021-03-08 23:36:53 -0600

    Begin Welcomepage

--- src/Startup/AMS.java
+++ src/Startup/AMS.java
@@ -5,23 +5,46 @@ package Startup;
   @author Blake Stadnyk; 11195866
  */
 
+import java.net.URL;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 import Entities.Passenger;
 import IO.IOAccess;
 import Singleton.AirportAccess;
 import Singleton.PassengerMapAccess;
 import Command.*;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * Models an Airport management program.
  * Methods to control the operation of the system
  */
-public class AMS {
+public class AMS extends Application implements Initializable{
+
+    @FXML
+    TextField airportname;
+    @FXML
+    Spinner<Integer> maxgates;
+    @FXML
+    TextField username;
+    @FXML
+    TextField password;
+
+
     /**
      * Instantiates a new Airport System.
      */
-    AMS() {
+    /*AMS() {
         String AirportName;
         int firstLabel, lastLabel;
 
@@ -42,6 +65,20 @@ public class AMS {
                 IOAccess.getInstance().outputString(e.getMessage());
             }
         } while (true);
+   }*/
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("AMS.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Airport Management System");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        maxgates.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,100));
     }
 
     /**
@@ -50,22 +87,24 @@ public class AMS {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-        new AMS();
-        while(true) {
-            IOAccess.getInstance().outputString("MAIN MENU: Make a selection from the following options:");
-            String[] choices = {"\t0: quit",
-                                "\t1: add a new passenger to the system",
-                                "\t2: add a new flight to the system",
-                                "\t3: view the flights in the system",
-                                "\t4: display the empty stalls in the Entities.Flight",
-                                "\t5: assign a passenger a stall",
-                                "\t6: release a passenger",
-                                "\t7: drop employee-passenger association",
-                                "\t8: display the current state of the system"};
+        launch(args);
 
-            int choice = IOAccess.getInstance().readChoice(choices);
+        //new AMS();
+        //while(true) {
+         //   IOAccess.getInstance().outputString("MAIN MENU: Make a selection from the following options:");
+        //    String[] choices = {"\t0: quit",
+         //                       "\t1: add a new passenger to the system",
+         //                       "\t2: add a new flight to the system",
+        //                        "\t3: view the flights in the system",
+          //                      "\t4: display the empty stalls in the Entities.Flight",
+         //                       "\t5: assign a passenger a stall",
+          //                      "\t6: release a passenger",
+          //                      "\t7: drop employee-passenger association",
+         //                       "\t8: display the current state of the system"};
 
-            Command[] commands = new Command[9];
+         //   int choice = IOAccess.getInstance().readChoice(choices);
+
+         //   Command[] commands = new Command[9];
             //commands[0] = new SystemState();
            // commands[1] = new AddPassenger();
            // commands[2] = new AddFlight();
@@ -78,12 +117,12 @@ public class AMS {
             //commands[7] = new DropAssociation();
             //commands[8] = new SystemState();
 
-            commands[choice].execute();
-            if (choice == 0) {
-                IOAccess.getInstance().outputString("Goodbye");
-                return;
-            }
-        }
+         //   commands[choice].execute();
+         //   if (choice == 0) {
+         //       IOAccess.getInstance().outputString("Goodbye");
+          //      return;
+          //  }
+       // }
     }
 
     /**
@@ -116,4 +155,5 @@ public class AMS {
 
       return outString;
     }
+
 }
