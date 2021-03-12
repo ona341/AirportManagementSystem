39920a8528e3ab492ffc7d01ef10503bc5e0f5ef
onyinyechiaghaizu <ona341@usask.ca>
2021-03-12 14:08:41 -0600

    Added Comments/javadocs to DeleteFlight

--- src/Command/DeleteFlight.java
+++ src/Command/DeleteFlight.java
@@ -11,10 +11,19 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * Delete a Flight in the system from the System
+ */
 public class DeleteFlight implements Command{
-
+    /**
+     * A list of flights
+     */
     private final ObservableList<Flight> selectedFlights;
 
+    /**
+     * Constructor
+     * @param selectedFlights a list of selected flights
+     */
     public DeleteFlight(ObservableList<Flight> selectedFlights) {
         this.selectedFlights = selectedFlights;
     }
@@ -24,6 +33,8 @@ public class DeleteFlight implements Command{
 
     @Override
     public void execute() {
+        //initializes a variable that is a list of flights with the list of flights from selectedFlights
+        // and removes them from FlightsAccess
         ObservableList<Flight> localCopy = FXCollections.observableArrayList(selectedFlights);
         FlightsAccess.getInstance().removeAll(localCopy);
         FlightsAccess.getSearchInstance().removeAll(localCopy);
@@ -33,14 +44,17 @@ public class DeleteFlight implements Command{
         String sql = "DELETE FROM flights WHERE flightNum = ?";
 
         try {
+            //opens the database connection
             Connection conn = dbConnection.getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql);
-
+            //for each flight in the localCopy list free the stall with the flights gate integer
+            //and set the PreparedStatement parameter index to the flight number of the flight
             for (Flight flight : localCopy) {
                 AirportAccess.getInstance().getGates().freeStall(flight.getGate());
                 pstmt.setString(1, flight.getFlightNumber());
                 pstmt.executeUpdate();
             }
+            //Closes the prepared statement
             pstmt.close();
 
         } catch (SQLException throwables) {
