dc638d754fa84d83e7a328c0827a2deeb036ddb8
Blake Stadnyk <blake.stadnyk@usask.ca>
2021-03-12 20:04:25 -0600

    Cleanup

--- src/main/java/Command/Command.java
+++ src/main/java/Command/Command.java
@@ -1,22 +1,4 @@
-package Command;/*
-  CMPT 270 A5
-  @author Blake Stadnyk; 11195866 - BJS645
- */
-
-/*
-  CMPT 270 Course material
-  Copyright (c) 2020
-  All rights reserved.
-
-  This document contains resources for homework assigned to students of
-  CMPT 270 and shall not be distributed without permission.  Posting this
-  file to a public or private website, or providing this file to a person
-  not registered in CMPT 270, constitutes Academic Misconduct, according
-  to the University of Saskatchewan Policy on Academic Misconduct.
-
-  Synopsis:
-     Starter file for Assignment 5
- */
+package Command;
 
 import java.sql.SQLException;
 
