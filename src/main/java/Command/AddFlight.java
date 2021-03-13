b33b9e1b0f77ddba209a4927d1f496a71c182c7d
Blake Stadnyk <blake.stadnyk@usask.ca>
2021-03-12 20:03:13 -0600

    Cleanup

--- src/main/java/Command/AddFlight.java
+++ src/main/java/Command/AddFlight.java
@@ -1,10 +1,5 @@
 package Command;
 
-/*
-  CMPT 270 A5Q5
-  @author Blake Stadnyk; 11195866 - BJS645
- */
-
 import java.sql.*;
 
 import AirportManager.AirportManagerController;
@@ -18,30 +13,6 @@ import javafx.fxml.FXML;
  * Allows the user to add a flight to the system.
  */
 public class AddFlight implements Command{
-    //@Override
-    /*public void execute() {
-        try {
-            Connection c =  dbConnection.getConnection();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        IOAccess.getInstance().outputString("Adding a new flight...");
-        String airline = IOAccess.getInstance().readString("Enter the airline:");
-        String number = IOAccess.getInstance().readString("Enter the flight number:");
-        String destination = IOAccess.getInstance().readString("Enter the destination:");
-        int capacity = IOAccess.getInstance().readInt("Enter the capacity:");
-
-        if (AirportAccess.getInstance().hasFlight(number)) {
-            IOAccess.getInstance().outputString("A flight with this number already exists in the system");
-        }
-        else {
-            Flight flight = new Flight(airline, number, destination, null, null);
-            AirportAccess.getInstance().assignFlightToGate(flight,AirportAccess.getInstance().availableGates().get(0));
-            IOAccess.getInstance().outputString("Flight created.");
-        }
-    }*/
-
     private final AirportManagerController airportManagerController;
 
     public AddFlight(AirportManagerController airportManagerController) {
@@ -75,6 +46,12 @@ public class AddFlight implements Command{
                             Time.valueOf(airportManagerController.time.getText()),
                             gate,
                             airportManagerController.capacity.getValue());
+
+            if (AirportAccess.getInstance().getGates().hasEntity(flight)) {
+                //TODO handle error
+            }
+
+
             FlightsAccess.getInstance().add(flight);
             AirportAccess.getInstance().getGates().assignEntityToStall(flight, gate);
 
