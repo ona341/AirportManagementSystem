e63f2e1586269378bb45b33acb417ccc4533bcb6
benphan <benphan6320@gmail.com>
2021-03-18 03:39:26 -0600

    Added some constraints to the view daily task feature for airport employee.

--- src/main/java/Command/viewEmployeeSchedule.java
+++ src/main/java/Command/viewEmployeeSchedule.java
@@ -4,12 +4,16 @@ import Singleton.dbConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 import java.net.URL;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class viewEmployeeSchedule implements Initializable {
@@ -45,21 +49,27 @@ public class viewEmployeeSchedule implements Initializable {
 
     }
 
+    @FXML
     public void check(ActionEvent event) {
         try {
             String sql = "SELECT * FROM workSchedule WHERE employeeId ='" + employeeId.getText() + "'";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            ResultSet rs = pstmt.executeQuery();
+            Statement pstmt = conn.createStatement();
+            ResultSet rs = pstmt.executeQuery(sql);
 
-            sun1.setText(rs.getString(2));
-            mon1.setText(rs.getString(3));
-            tues1.setText(rs.getString(4));
-            wed1.setText(rs.getString(5));
-            thur1.setText(rs.getString(6));
-            fri1.setText(rs.getString(7));
-            sat1.setText(rs.getString(8));
+            if (rs.next()) {
+                sun1.setText(rs.getString(2));
+                mon1.setText(rs.getString(3));
+                tues1.setText(rs.getString(4));
+                wed1.setText(rs.getString(5));
+                thur1.setText(rs.getString(6));
+                fri1.setText(rs.getString(7));
+                sat1.setText(rs.getString(8));
+            } else {
+                notifyError();
+            }
 
             pstmt.close();
+
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -69,8 +79,18 @@ public class viewEmployeeSchedule implements Initializable {
         employeeId.clear();
     }
 
+    /**
+     * Checks the format of the inputted time for validity
+     */
+    private void notifyError() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Invalid employee ID");
+        alert.setContentText("Please fill in the correct employee ID");
+        alert.showAndWait();
+    }
+
     @FXML
-    public void done(ActionEvent event) {
+    public void done(ActionEvent event) throws SQLException {
         ((Button) event.getSource()).getScene().getWindow().hide();
     }
 }
